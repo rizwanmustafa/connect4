514593544edcdb00587936129a7cc693476e20f2
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2021-10-16 12:20:42 +0500

    Method for creating empty board

---
+++ program.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from typing import List
+
+def create_empty_board() -> List[List[bool]]:
+	board = []
+
+	for i in range(6):
+		board.append([None] * 7)
+
+	return board
+
+# Debug statements
+board = create_empty_board()
